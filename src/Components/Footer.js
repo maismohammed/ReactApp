7d282bef3814df2fa8bfa49e2679a98e27b40920
maismohammed <68110333+maismohammed@users.noreply.github.com>
2023-07-29 18:09:26 +0300

    HeaderHomeFooter

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h2>Footer</h2>
+            <p></p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+
+function Header() {
+  return (
+      <div className="header">
+          <h2>Header</h2>
+          <p></p>
+      </div>
+  );
+};
+
+export default Header;
